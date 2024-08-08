96ed067fbd0c9629f072db93356ec3a76d006239
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-08-08 14:46:12 -0400

    Create Ship and Pirate Classes

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,16 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+
+  @@prirates = []
+
+  def initialize(name, weight, height)
+    @name = name
+    @weight = weight
+    @height = height
+    @@prirates << self
+  end
+
+  def self.all
+    @@prirates
+  end
 end
